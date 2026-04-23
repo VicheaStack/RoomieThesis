79431bf2e3494c27063486feaba3c1322b23a78d
Vichea leng <vichealeng45@gmail.com>
2026-04-23 20:59:15 +0700

    feat: add CDN and messaging services

---
+++ src/main/java/org/roomrental/group/RoomieHub/serviceImpl/CloudinaryService.java
@@ -0,0 +1,32 @@
+package org.roomrental.group.RoomieHub.serviceImpl;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImage(MultipartFile file) {
+
+        try {
+            Map result = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    Map.of("folder", "roomiehub")
+            );
+
+            return result.get("secure_url").toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Image upload failed");
+        }
+    }
+}
