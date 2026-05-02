a15ef73bffb09012ca9b637f115da1db0c0cb9b2
Vichea leng <vichealeng45@gmail.com>
2026-05-02 22:20:03 +0700

    Add JWT auth converter config

---
+++ src/main/java/org/roomrental/group/RoomieHub/config/JwtAuthConverterConfig.java
@@ -0,0 +1,22 @@
+package org.roomrental.group.RoomieHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collections;
+
+@Configuration
+public class JwtAuthConverterConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            String role = jwt.getClaimAsString("role");
+            return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+        });
+        return converter;
+    }
+}
